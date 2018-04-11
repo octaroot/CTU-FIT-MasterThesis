c6f0e37748b9be112a2a4f164bb4b161f08cad7f
Martin Černáč <cernac@cesnet.cz>
2018-04-11 11:41:13 +0200

    Argument parsing & basic main() structure

---
+++ code/common.h
@@ -0,0 +1,6 @@
+#ifndef CODE_COMMON_H
+#define CODE_COMMON_H
+
+#define PROGRAM_VERSION "1.0.0"
+
+#endif //CODE_COMMON_H
--- code/main.c
+++ code/main.c
@@ -1,6 +1,104 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <getopt.h>
+#include <unistd.h>
+#include <time.h>
+#include <stdbool.h>
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
+#include "common.h"
+
+static void pritnHelp(char *programName) {
+    fprintf(stderr, "%s %s.\n", programName, PROGRAM_VERSION);
+    fprintf(stderr, "  -v               print program version and exit\n");
+    fprintf(stderr, "  -h               print help and exit\n");
+    fprintf(stderr, "  -t <server>      test connectivity to server and exit\n");
+    fprintf(stderr, "  -s               run in server mode\n");
+    fprintf(stderr, "  -c <server>      run in client mode, connect to server ip/hostname\n");
+    exit(EXIT_SUCCESS);
+}
+
+static void printVersion() {
+    fprintf(stderr, "%s, built on %s %s", PROGRAM_VERSION, __DATE__, __TIME__);
+    exit(EXIT_SUCCESS);
+}
+
+static void handleSignal(int sig) {
+    fprintf(stderr, "Detected SIGTERM/SIGINT, closing connection\n");
+
+    //TODO
+}
+
+
+int main(int argc, char **argv) {
+
+    bool justTestConnectivity = false;
+    bool serverMode = false;
+    bool clientMode = false;
+    int parameter;
+
+    char *servername = NULL;
+
+    while ((parameter = getopt(argc, argv, "vhtsc:")) != -1) {
+        switch (parameter) {
+            case 'v':
+                printVersion();
+                break;
+            case 'h':
+                pritnHelp(argv[0]);
+                break;
+            case 't':
+                justTestConnectivity = true;
+                servername = optarg;
+                break;
+            case 's':
+                serverMode = true;
+                break;
+            case 'c':
+                clientMode = true;
+                servername = optarg;
+                break;
+            default:
+                fprintf(stderr, "Unknown or missing operand. Refer to program help (-h)\n");
+                exit(EXIT_FAILURE);
+        }
+    }
+
+
+    if (optind < argc) {
+        for (int i = optind; i < argc; ++i) {
+            fprintf(stderr, "Unknown or missing operand \"%s\". Refer to program help (-h)\n", argv[i]);
+        }
+        exit(EXIT_FAILURE);
+    }
+
+    if (clientMode ^ serverMode == false && !justTestConnectivity) {
+        fprintf(stderr, "Select either server (-s) mode or client (-c <server>) mode\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if (geteuid() != 0) {
+        fprintf(stderr, "This program must be run as root\n");
+        exit(EXIT_FAILURE);
+    }
+
+
+    signal(SIGINT, handleSignal);
+    signal(SIGTERM, handleSignal);
+
+    srand(time(NULL));
+
+    if (justTestConnectivity) {
+        //TODO
+        exit(EXIT_SUCCESS);
+    }
+
+    if (serverMode) {
+        //TODO
+    } else {
+        //TODO
+    }
+
+
+    exit(EXIT_SUCCESS);
 }
