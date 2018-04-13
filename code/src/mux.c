b741c7477f25228676c131717a818fdd11036b1a
Martin Černáč <me@martincernac.cz>
2018-04-13 12:25:14 +0200

    Add multiplexer (via OpenMP tasks + plugins)

--- code/src/mux.c
+++ code/src/mux.c
@@ -1,4 +1,33 @@
-//
-// Created by martin on 13.4.18.
-//
+//tady budou nejak zadratovany odkazy na jednotlive pluginy, takovy "interface" v C.
+//taky se tu budou resit vlakna
 
+#include <omp.h>
+#include <stdio.h>
+#include <unistd.h>
+#include "mux.h"
+
+#include "../plugins/icmp/icmp.h"
+#include "../plugins/dns/dns.h"
+
+#define PLUGIN_COUNT (sizeof(plugins) / sizeof(plugin))
+
+plugin plugins[] = {
+		{ICMPGetVersion},
+		{DNSGetVersion},
+};
+
+void mux_start()
+{
+
+#pragma omp parallel num_threads(PLUGIN_COUNT)
+#pragma omp single nowait
+	for (int i = 0; i < PLUGIN_COUNT; ++i)
+	{
+#pragma omp task
+		{
+
+			//debug print
+			printf("%d, thread: %d, %s\n", i, omp_get_thread_num(), plugins[i].getVersion());
+		};
+	}
+}
--- code/src/mux.h
+++ code/src/mux.h
@@ -1,8 +1,13 @@
-//
-// Created by martin on 13.4.18.
-//
-
 #ifndef CODE_MUX_H
 #define CODE_MUX_H
+
+#include <stdbool.h>
+
+typedef struct plugin
+{
+	const char *(*getVersion)();
+} plugin;
+
+void mux_start();
 
 #endif //CODE_MUX_H
