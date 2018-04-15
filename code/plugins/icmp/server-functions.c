963ce0ceb1700ea6a2199e063417a3740b1adab0
Martin Černáč <me@martincernac.cz>
2018-04-15 21:43:50 +0200

    Add ICMP server-side functions (handler support)

--- code/plugins/icmp/server-functions.c
+++ code/plugins/icmp/server-functions.c
@@ -1,5 +1,60 @@
-//
-// Created by martin on 15.4.18.
-//
+#include <memory.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 #include "server-functions.h"
+#include "packet.h"
+#include "icmp.h"
+
+uint16_t NATSequenceNumbers[ICMP_NAT_PACKET_COUNT];
+int NATSequenceNumberIdx = 0;
+
+void ICMPHandlConnectionRequest(int socketFD, uint32_t endpoint, struct ICMPEchoMessage * request)
+{
+	struct ICMPEchoMessage msg;
+	msg.size = 0;
+	msg.type = ICMP_ECHO_REPLY;
+	msg.seq = request->seq;
+
+	if (pluginState.connected)
+	{
+		msg.packetType = ICMP_CONNECTION_REJECT;
+
+		uint16_t oldID = ICMPIDNumber;
+		ICMPSendEcho(socketFD, endpoint, &msg);
+		ICMPIDNumber = oldID;
+	}
+	else
+	{
+		msg.packetType = ICMP_CONNECTION_ACCEPT;
+
+		ICMPSequenceNumber = request->seq;
+		ICMPIDNumber = request->id;
+
+		pluginState.connected = true;
+		pluginState.endpoint = endpoint;
+	}
+
+	msg.seq = ICMPSequenceNumber++;
+
+
+}
+
+void ICMPHandleNATPacket(int socketFD, uint32_t endpoint, struct ICMPEchoMessage * request)
+{
+	if (!pluginState.connected || pluginState.endpoint != endpoint)
+		return;
+
+	NATSequenceNumbers[NATSequenceNumberIdx++] = request->seq;
+	NATSequenceNumberIdx %= ICMP_NAT_PACKET_COUNT;
+}
+
+void ICMPHandleKeepAlive(int socketFD, uint32_t endpoint, struct ICMPEchoMessage * request)
+{
+	if (!pluginState.connected || pluginState.endpoint != endpoint)
+		return;
+
+	request->type = ICMP_ECHO_REPLY;
+
+	ICMPSendEcho(socketFD, endpoint, request);
+}
--- code/plugins/icmp/server-functions.h
+++ code/plugins/icmp/server-functions.h
@@ -1,8 +1,21 @@
-//
-// Created by martin on 15.4.18.
-//
-
 #ifndef CODE_SERVER_FUNCTIONS_H
 #define CODE_SERVER_FUNCTIONS_H
 
+#include <stdint.h>
+#include <stdbool.h>
+#include "packet.h"
+
+extern uint16_t ICMPSequenceNumber, ICMPIDNumber;
+
+extern int _ICMPSocketFD;
+
+extern struct ICMPPluginState pluginState;
+
+void ICMPHandlConnectionRequest(int socketFD, uint32_t endpoint, struct ICMPEchoMessage * request);
+
+void ICMPHandleNATPacket(int socketFD, uint32_t endpoint, struct ICMPEchoMessage * request);
+
+void ICMPHandleICMPData(struct ICMPEchoMessage *msg);
+
+void ICMPHandleKeepAlive(int socketFD, uint32_t endpoint, struct ICMPEchoMessage * request);
 #endif //CODE_SERVER_FUNCTIONS_H
