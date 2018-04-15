60965be1c61964f7074f72898bd3277f76afdb97
Martin Černáč <me@martincernac.cz>
2018-04-15 16:43:02 +0200

    Add ICMP packet handling functions

---
+++ code/plugins/icmp/packet.c
@@ -0,0 +1,97 @@
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <netinet/ip.h>
+#include <netinet/ip_icmp.h>
+
+#include "packet.h"
+
+uint16_t ICMPSequenceNumber, ICMPIDNumber;
+
+int ICMPReceiveEcho(int socketFD, uint32_t *from, struct ICMPEchoMessage *msg)
+{
+	char buffer[ICMP_SOCKET_MTU];
+
+	struct sockaddr_in server;
+
+	int receivedSize = recvfrom(socketFD, buffer, ICMP_SOCKET_MTU, 0, (struct sockaddr *) &server, sizeof(server));
+
+	if (receivedSize < 0)
+	{
+		fprintf(stderr, "Unable to receive an ICMP packet: %s\n", strerror(errno));
+		return 1;
+	}
+
+	if (receivedSize < (int) sizeof(struct iphdr) + (int) sizeof(struct icmphdr))
+	{
+		fprintf(stderr, "Received malformed ICMP packet: %s\n", strerror(errno));
+		return 1;
+	}
+
+	struct icmphdr *header = (struct icmphdr *) (buffer + sizeof(struct iphdr));
+
+	if ((header->type != ICMP_TYPE_ECHO_REPLY && header->type != ICMP_TYPE_ECHO_REQUEST) || header->code != 0)
+		return 1;  /* unexpected packet type. */
+
+	msg->size = receivedSize - sizeof(struct iphdr) - sizeof(struct icmphdr);
+	msg->type = header->type == 0;
+	msg->id = ntohs(header->un.echo.id);
+	msg->seq = ntohs(header->un.echo.sequence);
+
+	*from = ntohl(server.sin_addr.s_addr);
+
+	return 0;
+}
+
+void ICMPSocketClose(int socketFD)
+{
+	if (socketFD > 0)
+		close(socketFD);
+}
+
+int ICMPSendEcho(int socketFD, uint32_t to, struct ICMPEchoMessage *msg)
+{
+	char buffer[ICMP_SOCKET_MTU];
+
+	struct icmphdr *header = (struct icmphdr *) (buffer);
+	header->type = msg->type;
+	header->code = 0;
+	header->un.echo.id = htons(ICMPIDNumber);
+	header->un.echo.sequence = htons(msg->seq);
+	header->checksum = 0;
+
+	struct ICMPPacketHeader *customHeader = (struct ICMPPacketHeader*)(buffer + sizeof(struct icmphdr));
+	memcpy(customHeader->magic, ICMP_PACKET_MAGIC, sizeof(customHeader->magic));
+	customHeader->type = ICMP_CONNECTION_REQUEST;
+
+	memcpy(buffer + sizeof(struct icmphdr) + sizeof(struct ICMPPacketHeader), msg->buffer, msg->size);
+
+	struct sockaddr_in server;
+	server.sin_family = AF_INET;
+	server.sin_addr.s_addr = htonl(to);
+
+	int sentSize = sendto(socketFD, buffer, msg->size + sizeof(struct ICMPPacketHeader) + sizeof(struct icmphdr), 0, (struct sockaddr *) &server, sizeof(server));
+
+	if (sentSize < 0)
+	{
+		fprintf(stderr, "Unable to send an ICMP packet: %s\n", strerror(errno));
+		return 1;
+	}
+
+	return 0;
+}
+
+int ICMPSocketOpen()
+{
+	int ICMPSocket = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
+
+	if (ICMPSocket < 0)
+	{
+		fprintf(stderr, "Unable to open an ICMP socket: %s\n", strerror(errno));
+		return 1;
+	}
+
+	return ICMPSocket;
+}
+
---
+++ code/plugins/icmp/packet.h
@@ -0,0 +1,61 @@
+#ifndef CODE_PACKET_H
+#define CODE_PACKET_H
+
+#include <stdint.h>
+#include <netinet/ip.h>
+#include <netinet/ip_icmp.h>
+
+#include "../../src/tun-device.h"
+
+#define ICMP_SOCKET_MTU  (TUN_DEVICE_MTU + sizeof(struct ICMPPacketHeader) + sizeof(struct iphdr) + sizeof(struct icmphdr))
+
+typedef enum ICMP_PACKET_TYPE
+{
+	ICMP_PUNCHTHROUGH,
+	ICMP_CONNECTION_REQUEST,
+	ICMP_CONNECTION_ACCEPT,
+	ICMP_CONNECTION_REJECT,
+	ICMP_KEEPALIVE,
+	ICMP_DATA
+
+} ICMP_PACKET_TYPE;
+
+typedef enum ICMP_TYPE
+{
+	ICMP_ECHO_REQUEST = 0x08,
+	ICMP_ECHO_REPLY = 0x0,
+
+} ICMP_TYPE;
+
+typedef struct ICMPPacketHeader
+{
+	uint8_t magic[4];
+	uint8_t type; /* ICMP_PACKET_TYPE */
+} ICMPPacketHeader;
+
+typedef struct ICMPEchoMessage
+{
+	int size;
+	ICMP_TYPE type;
+	ICMP_PACKET_TYPE packetType;
+	char buffer[TUN_DEVICE_MTU];
+	uint16_t id;
+	uint16_t seq;
+} ICMPEchoMessage;
+
+#define ICMP_PACKET_MAGIC "CVUT"
+
+#define ICMP_TYPE_ECHO_REQUEST    8
+#define ICMP_TYPE_ECHO_REPLY    0
+
+
+
+int ICMPSocketOpen();
+
+int ICMPSendEcho(int socketFD, uint32_t to, struct ICMPEchoMessage *msg);
+
+int ICMPReceiveEcho(int socketFD, uint32_t *from, struct ICMPEchoMessage *msg);
+
+void ICMPSocketClose(int socketFD);
+
+#endif //CODE_PACKET_H
